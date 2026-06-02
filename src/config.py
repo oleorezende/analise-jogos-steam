6ad1436c57136a90b3e0d294526297a4f3e58982
Leonardo Rezende <engleonardorezende@gmail.com>
2026-06-02 09:59:01 -0300

    feat: add reports path configuration

--- src/config.py
+++ src/config.py
@@ -6,6 +6,9 @@ DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
+REPORTS_DIR = PROJECT_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
+
 RAW_STEAM_GAMES_FILE = RAW_DATA_DIR / "steam_games.csv"
 PROCESSED_STEAM_GAMES_FILE = PROCESSED_DATA_DIR / "steam_games_clean.csv"
 PROCESSED_STEAM_GENRES_FILE = PROCESSED_DATA_DIR / "steam_game_genres.csv"
