9fdb3f9b4c44c0862ab85c1220250db19a1e6aa5
Leonardo Rezende <engleonardorezende@gmail.com>
2026-06-02 10:00:02 -0300

    feat: include visualizations in full pipeline

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,4 +1,5 @@
 from src.build_database import main as build_database
+from src.create_visualizations import main as create_visualizations
 from src.export_powerbi_tables import main as export_powerbi_tables
 from src.prepare_data import main as prepare_data
 
@@ -7,6 +8,7 @@ def main() -> None:
     prepare_data()
     build_database()
     export_powerbi_tables()
+    create_visualizations()
 
 
 if __name__ == "__main__":
